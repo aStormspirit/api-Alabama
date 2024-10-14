d02a2e562115e2dbe0d5a8c3403d28d0bcc78570
Vladimir <shinkarenkovova@gmail.com>
2024-10-14 12:28:19 +0300

    dsd

--- scripts/NorthCalifornia.py
+++ scripts/NorthCalifornia.py
@@ -1,15 +1,16 @@
 from seleniumbase import BaseCase
 
 class NorthCalifornia(BaseCase):
-    # при выполнение 3 запросов в минуту просиходит проверка
+
     USER_DATA = {
         'name': 'The instruction LLC',
         'firstName': 'Stan',
         'lastName': 'Smith',
-        'address': '1022 McDaniel St SW',
-        'city': 'Atlanta',
-        'state': 'Georgia',
-        'zip': '30310',
+        'address': '2941 23rd St',
+        'city': 'Sacramento',
+        'state': 'California',
+        'zip': '95818',
+        "email": "ddgkevinedwards435@gmail.com",
     }
 
     LOGIN = {
@@ -23,6 +24,17 @@ class NorthCalifornia(BaseCase):
         self.page2()
         self.page3()
         self.page4()
+        self.page5()
+        self.page6()
+        self.page7()
+        self.page8()
+        self.page9()
+        self.page10()
+        self.page11()
+        self.page12()
+        self.page13()
+        self.page14()
+        self.page15()
         
 
     def setUp(self):
@@ -45,18 +57,74 @@ class NorthCalifornia(BaseCase):
         self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
 
     def page3(self):
-        self.click_chain(['/html/body/div[3]/main/article/section/form/div[6]/div[2]/div[1]/select', '/html/body/div[3]/main/article/section/form/div[6]/div[2]/div[1]/select/option[10]'], by='xpath')
-        self.click_chain(['/html/body/div[3]/main/article/section/form/div[6]/div[2]/div[2]/select', '/html/body/div[3]/main/article/section/form/div[6]/div[2]/div[2]/select/option[2]'], by='xpath')
-        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button')
+        self.click('/html/body/div[3]/main/article/section/form/div[7]/div[1]/fieldset/ul/li[1]/label', by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
 
     def page4(self):
-        self.click('/html/body/div[3]/main/article/section/form/div[7]/div[1]/fieldset/ul/li[1]/label')
-        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button')
+        self.type('/html/body/div[3]/main/article/section/form/div[8]/div[1]/input', self.USER_DATA['email'], by='xpath')
+        self.click_chain(['/html/body/div[3]/main/article/section/form/div[8]/div[2]/select', '/html/body/div[3]/main/article/section/form/div[8]/div[2]/select/option[3]'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[8]/div[5]/div/div[1]/input', self.USER_DATA['firstName'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[8]/div[5]/div/div[3]/input', self.USER_DATA['lastName'], by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
 
     def page5(self):
-        self.type('/html/body/div[3]/main/article/section/form/div[8]/div[1]/input')
-    
+        self.type('/html/body/div[3]/main/article/section/form/div[9]/section/div[1]/textarea', self.USER_DATA['address'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[9]/section/div[2]/div[1]/input', self.USER_DATA['city'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[9]/section/div[2]/div[3]/input', self.USER_DATA['zip'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[9]/section/div[3]/div[1]/input', self.USER_DATA['state'], by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[9]/section/div[4]/fieldset/ul/li[1]/label', by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
 
+    def page6(self):
+        self.click_chain(['/html/body/div[3]/main/article/section/form/div[11]/div[1]/select', '/html/body/div[3]/main/article/section/form/div[11]/div[1]/select/option[3]'], by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
+
+    def page7(self):
+        #user Agent form
+        self.click('/html/body/div[3]/main/article/section/form/div[14]/section/div[5]/div[2]/span[1]/button', by='xpath')
+        self.click_chain(['/html/body/div[3]/main/article/section/form/div[14]/div/div/div[2]/select', '/html/body/div[3]/main/article/section/form/div[14]/div/div/div[2]/select/option[2]'], by='xpath')
+        self.click_chain(['/html/body/div[3]/main/article/section/form/div[14]/div/div/div[3]/div[1]/div[1]/select', '/html/body/div[3]/main/article/section/form/div[14]/div/div/div[3]/div[1]/div[1]/select/option[3]'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[14]/div/div/div[3]/div[3]/div[1]/input', self.USER_DATA['firstName'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[14]/div/div/div[3]/div[3]/div[3]/input', self.USER_DATA['lastName'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[14]/div/div/div[3]/div[4]/input', self.USER_DATA['address'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[14]/div/div/div[3]/div[5]/div[1]/input', self.USER_DATA['city'], by='xpath')
+        self.type('/html/body/div[3]/main/article/section/form/div[14]/div/div/div[3]/div[5]/div[3]/input', self.USER_DATA['zip'], by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[14]/div/div/div[4]/button[2]', by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[14]/section/div[5]/div[1]/fieldset/ul/li/label', by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
+
+    def page8(self):
+        self.sleep(2)
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
+    
+    def page9(self):
+        self.sleep(2)
+        self.click_chain(['/html/body/div[3]/main/article/section/form/div[20]/div[7]/select', '/html/body/div[3]/main/article/section/form/div[20]/div[7]/select/option[2]'], by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
+
+    def page10(self):
+        self.sleep(5)
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
+
+    def page11(self):
+        self.type('/html/body/div[3]/main/article/section/form/div[22]/div[2]/input', '10/14/2024', by='xpath')
+        self.click_chain(['/html/body/div[3]/main/article/section/form/div[22]/div[5]/table/tbody/tr[1]/td[1]/select', '/html/body/div[3]/main/article/section/form/div[22]/div[5]/table/tbody/tr[1]/td[1]/select/option[2]'], by='xpath')
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
+
+    def page12(self):
+        self.click_chain(['/html/body/div[3]/main/article/section/form/div[23]/div[2]/select', '/html/body/div[3]/main/article/section/form/div[23]/div[2]/select/option[3]'], by='xpath')
+
+    def page13(self):
+        self.sleep(2)
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
+
+    def page14(self):
+        self.sleep(2)
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')
+
+    def page15(self):
+        self.sleep(2)
+        self.click('/html/body/div[3]/main/article/section/form/div[30]/div/button', by='xpath')  
 
 
 if __name__ == "__main__":
